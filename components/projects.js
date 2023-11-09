682970744ee997ea8db1c6a256d8f66c4732940d
Kim Rune Møller <kimrune@hotmail.no>
2023-11-09 23:30:36 +0100

    extracts projects into their own components

--- components/projects.js
+++ components/projects.js
@@ -1,183 +1,72 @@
-import Image from "next/image";
-import Link from "next/link";
+import ProjectItem from "./projectItem";
 import styles from "@/styles/projects.module.css";
 
 export default function Projects() {
+  const projects = [
+    {
+      title: "Kunnskaps- og Næringsparken Senja Website",
+      description:
+        "This is a website I made for a local building housing several businesses. It's a custom made Wordpress theme with custom fields og posts. Styling is done with Bootstrap.",
+      image: "/knpsenja.png",
+      link: "https://knpsenja.no",
+      github: "",
+    },
+    {
+      title: "Portfolio (this webpage)",
+      description: "Portfolio website made with React/Next.js",
+      image: "/portfolio.png",
+      link: "",
+      github: "https://github.com/kimrm/portfolio",
+    },
+    {
+      title: "Social Media Client",
+      description:
+        "This is a school project for a frontend developer course. It's a fictional social media client. Made with Vanilla JS, HTML and CSS.",
+      image: "/social_app.png",
+      link: "",
+      github: "https://github.com/kimrm/JS2-CA",
+    },
+    {
+      title: "Møllers Kitchen Food blog",
+      description:
+        "Food blog made with vanilla javascript, HTML and CSS. Content is served from a headless Wordpress installation. This project was a project exam for a frontend developer course.",
+      image: "/foodblog.png",
+      link: "https://astounding-jalebi-1c1cf5.netlify.app/",
+      github: "https://github.com/kimrm/foodblog",
+    },
+    {
+      title: "Rainy Days",
+      description:
+        "This is a project I made for a frontend developer course assignment. It's a fictional webshop for rainwear. Made with vanilla javascript, HTML and CSS. Integrated with Wordpress and wooCommerce API.",
+      image: "/rainy_days.png",
+      link: "https://splendid-custard-e9a90d.netlify.app/",
+      github: "https://github.com/kimrm/rainy-days-ca",
+    },
+    {
+      title: "Community Science Museum",
+      description:
+        "This was a semester project for a frontend developer course. It's a fictional website for a science museum. Made with vanilla javascript, HTML and CSS.",
+      image: "/cosmu_site.png",
+      link: "https://joyful-kheer-f14cfb.netlify.app/",
+      github: "https://github.com/kimrm/cosmu",
+    },
+    {
+      title: "Movie List",
+      description:
+        "This is a school project for a frontend developer course. The site fetches data the iMDB top 100 ranked movies API.",
+      image: "/movielist.png",
+      link: "https://superb-hummingbird-948cfe.netlify.app/",
+      github: "https://github.com/kimrm/movienight",
+    },
+  ];
+
   return (
     <div className={styles.projects_section}>
       <h2 className={styles.heading_2}>My Latest projects</h2>
       <div className={styles.projects}>
-        <div className={styles.project}>
-          <Image
-            src="/knpsenja.png"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            className={styles.project_image}
-          />
-          <div className={styles.project_content}>
-            <h3 className={styles.heading_3}>
-              Kunnskaps- og Næringsparken Senja Website
-            </h3>
-            <div className={styles.description}>
-              <p>
-                This is a website I made for a local building housing several
-                businesses. It&apos;s a custom made Wordpress theme with custom
-                fields og posts. Styling is done with Bootstrap.
-              </p>
-            </div>
-            <div className={styles.links}>
-              <Link
-                href="https://knpsenja.no"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Visit site</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className={styles.project}>
-          <Image
-            src="/foodblog.png"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            className={styles.project_image}
-          />
-          <div className={styles.project_content}>
-            <h3 className={styles.heading_3}>Møllers Kitchen Food blog</h3>
-            <div className={styles.description}>
-              <p>
-                Food blog made with vanilla javascript, HTML and CSS. Content is
-                served from a headless Wordpress installation. This project was
-                a project exam for a frontend developer course.
-              </p>
-            </div>
-            <div className={styles.links}>
-              <Link
-                href="https://astounding-jalebi-1c1cf5.netlify.app/"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Visit site</span>
-              </Link>
-              <Link
-                href="https://github.com/kimrm/foodblog"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Github</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className={styles.project}>
-          <Image
-            src="/rainy_days.png"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            className={styles.project_image}
-          />
-          <div className={styles.project_content}>
-            <h3 className={styles.heading_3}>Rainy Days</h3>
-            <div className={styles.description}>
-              <p>
-                This is a project I made for a frontend developer course
-                assignment. It&apos;s a fictional webshop for rainwear. Made
-                with vanilla javascript, HTML and CSS. Integrated with Wordpress
-                and wooCommerce API.
-              </p>
-            </div>
-            <div className={styles.links}>
-              <Link
-                href="https://splendid-custard-e9a90d.netlify.app/"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Visit site</span>
-              </Link>
-              <Link
-                href="https://github.com/kimrm/rainy-days-ca"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Github</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className={styles.project}>
-          <Image
-            src="/cosmu_site.png"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            className={styles.project_image}
-          />
-          <div className={styles.project_content}>
-            <h3 className={styles.heading_3}>Community Science Museum</h3>
-            <div className={styles.description}>
-              <p>
-                This was a semester project for a frontend developer course.
-                It&apos;s a fictional website for a science museum. Made with
-                vanilla javascript, HTML and CSS.
-              </p>
-            </div>
-            <div className={styles.links}>
-              <Link
-                href="https://joyful-kheer-f14cfb.netlify.app/"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Visit site</span>
-              </Link>
-              <Link
-                href="https://github.com/kimrm/cosmu"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Github</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.project}>
-          <Image
-            src="/movielist.png"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            className={styles.project_image}
-          />
-          <div className={styles.project_content}>
-            <h3 className={styles.heading_3}>Movie List</h3>
-            <div className={styles.description}>
-              <p>
-                This is a school project for a frontend developer course. The
-                site fetches data the iMDB top 100 ranked movies API.
-              </p>
-            </div>
-            <div className={styles.links}>
-              <Link
-                href="https://superb-hummingbird-948cfe.netlify.app/"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Visit site</span>
-              </Link>
-              <Link
-                href="https://github.com/kimrm/movienight"
-                target="_blank"
-                className={styles.link}
-              >
-                <span>Github</span>
-              </Link>
-            </div>
-          </div>
-        </div>
+        {projects.map((project, index) => (
+          <ProjectItem key={index} project={project} />
+        ))}
       </div>
     </div>
   );
